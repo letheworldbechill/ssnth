5874ee0d1af160b3c65d2354031215d8401cfcbd
Severin Leander <gasserseverin1@gmail.com>
2025-12-05 13:02:03 +0100

    Implement service worker for caching resources
    This service worker caches specified resources for offline access and handles fetch events.

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "schritte-app-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
